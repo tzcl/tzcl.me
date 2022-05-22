15258be0b586473f55d7433c7de0ceb7ea533ee1
Toby Law <toby@tzcl.me>
2022-05-22 12:44:41 +1000

    feat(robot): robot animation working!

--- public/blog/eloquentjs/robot.js
+++ public/blog/eloquentjs/robot.js
@@ -1,13 +1,6 @@
-function elt(name, props, ...children) {
-  let dom = document.createElement(name);
-  if (props) Object.assign(dom, props);
-  for (let child of children) {
-    if (typeof child != "string") dom.appendChild(child);
-    else dom.appendChild(document.createTextNode(child));
-  }
-
-  return dom;
-}
+//
+// Algorithm
+//
 
 let active = null;
 
@@ -27,23 +20,132 @@ const places = {
 const placeKeys = Object.keys(places);
 const speed = 2;
 
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+//
+// State
+//
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+
+  static random(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+      let address = randomPick(Object.keys(roadGraph));
+      let place;
+      do {
+        place = randomPick(Object.keys(roadGraph));
+      } while (place == address);
+      parcels.push({ place, address });
+    }
+    return new VillageState("Post Office", parcels);
+  }
+}
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+//
+// View
+//
+
+function elt(name, props, ...children) {
+  let dom = document.createElement(name);
+  if (props) Object.assign(dom, props);
+  for (let child of children) {
+    if (typeof child != "string") dom.appendChild(child);
+    else dom.appendChild(document.createTextNode(child));
+  }
+
+  return dom;
+}
+
 class RobotAnimation {
   constructor(worldState, robot, robotState) {
     this.worldState = worldState;
     this.robot = robot;
     this.robotState = robotState;
     this.turn = 0;
+    this.parcels = [];
 
-    this.village = elt(
-      "div",
-      { style: "width: 400px" },
-      elt("img", {
-        style: "width: inherit",
-        src: "../eloquentjs/assets/village.png",
-      })
-    );
+    this.village = elt("img", {
+      style: "vertical-align: -8px",
+      src: "assets/village.png",
+    });
 
-    this.robot = elt(
+    this.robotElt = elt(
       "div",
       {
         style: `width: 40px; position: absolute; transition: left ${
@@ -53,24 +155,125 @@ top: 110px; left:162px;`,
       },
       elt("img", {
         style: "width: inherit",
-        src: "../eloquentjs/assets/robot.gif",
+        src: "assets/robot.gif",
       })
     );
+    this.robotElt.addEventListener("transitionend", () => this.updateParcels());
+
+    this.text = elt("span");
+    this.button = elt(
+      "button",
+      {
+        style: `
+color: white;
+background: #28b;
+border: none;
+border-radius: 2px;
+padding: 2px 5px;
+line-height: 1.1;
+font-family: sans-serif;
+font-size: 80%`,
+      },
+      "Stop"
+    );
+    this.button.addEventListener("click", () => this.clicked());
 
     this.dom = elt(
       "div",
       { style: "position: relative; line-height: 0.1; margin-left: 10px" },
       this.village,
-      this.robot
+      this.robotElt,
+      this.text,
+      this.button
     );
+
+    this.schedule();
+    this.updateView();
+    this.updateParcels();
+  }
+
+  tick() {
+    let { direction, memory } = this.robot(this.worldState, this.robotState);
+    this.worldState = this.worldState.move(direction);
+    this.robotState = memory;
+    this.turn++;
+    this.updateView();
+    if (this.worldState.parcels.length == 0) {
+      this.button.remove();
+      this.text.textContent = ` Finished after ${this.turn} turns`;
+      this.robotElt.firstChild.src = "assets/robot.png";
+    } else {
+      this.schedule();
+    }
+  }
+
+  schedule() {
+    this.timeout = setTimeout(() => this.tick(), 1000 / speed);
+  }
+
+  updateView() {
+    let pos = places[this.worldState.place];
+    this.robotElt.style.top = pos.y - 38 + "px";
+    this.robotElt.style.left = pos.x - 16 + "px";
+
+    this.text.textContent = ` Turn ${this.turn}`;
+  }
+
+  updateParcels() {
+    // empty parcels
+    while (this.parcels.length) this.parcels.pop().remove();
+
+    let heights = {};
+    for (let { place, address } of this.worldState.parcels) {
+      let height = heights[place] || (heights[place] = 0);
+      heights[place] += 14;
+
+      let offset = placeKeys.indexOf(address) * 16;
+      let parcel = elt("div", {
+        style: `
+position: absolute;
+height: 16px;
+width: 16px;
+background-image: url(assets/parcel.png);
+background-position: 0 -${offset}px`,
+      });
+
+      if (place == this.worldState.place) {
+        parcel.style.top = 20 + height + "px";
+        parcel.style.left = "25px";
+        this.robotElt.appendChild(parcel);
+      } else {
+        let pos = places[place];
+        parcel.style.top = pos.y - 10 - height + "px";
+        parcel.style.left = pos.x - 5 + "px";
+        this.dom.appendChild(parcel);
+      }
+      this.parcels.push(parcel);
+    }
+  }
+
+  clicked() {
+    if (this.timeout == null) {
+      this.schedule();
+      this.button.textContent = "Stop";
+      this.robotElt.firstChild.src = "assets/robot.gif";
+    } else {
+      clearTimeout(this.timeout);
+      this.timeout = null;
+      this.button.textContent = "Start";
+      this.robotElt.firstChild.src = "assets/robot.png";
+    }
   }
 }
 
-let robotAnimation = new RobotAnimation();
-document.getElementById("pathfinding").appendChild(robotAnimation.dom);
+function runRobotAnimation(state, robot, memory) {
+  if (active && active.timeout != null) clearTimeout(active.timeout);
+  active = new RobotAnimation(state, robot, memory);
+}
 
-// This is tricky, come back to this
+//
+// Application
+//
 
-let sections = document.getElementsByTagName("section");
-alert(sections.length);
-sections.map((s) => s.addEventListener("onclick", console.log("clicked!", s)));
+let robotAnimation = new RobotAnimation(VillageState.random(), randomRobot, []);
+document.getElementById("root").appendChild(robotAnimation.dom);
