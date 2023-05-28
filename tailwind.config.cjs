4320ff4fc9a8058d58dae17946eb8f2ab3a2b069
Toby Law <toby@tzcl.me>
2023-05-28 21:33:18 +1000

    Fix up homepage content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,10 +9,11 @@ module.exports = {
         sky: "url('sky.jpg')",
       },
       fontFamily: {
-        lora: ["Lora", "serif"],
+        lora: ["Lora", ...defaultTheme.fontFamily.serif],
+        georgia: ["Georgia", ...defaultTheme.fontFamily.serif],
       },
       fontSize: {
-        title: "clamp(4em, 15vw + 1rem, 15em)",
+        title: "clamp(4em, 15vw + 1rem, 10em)",
       },
     },
   },
