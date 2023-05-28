0b6a091619f5159bbe696561ad3ecb72695043c1
Toby Law <toby@tzcl.me>
2023-05-28 22:06:41 +1000

    Fixed up favicons and layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,15 +5,20 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundColor: {
+        light: "#fffff8",
+        dark: "#151515",
+      },
       backgroundImage: {
         sky: "url('sky.jpg')",
+        roof: "url('roof.jpg')",
       },
       fontFamily: {
         lora: ["Lora", ...defaultTheme.fontFamily.serif],
         georgia: ["Georgia", ...defaultTheme.fontFamily.serif],
       },
       fontSize: {
-        title: "clamp(4em, 15vw + 1rem, 10em)",
+        title: "clamp(4em, 15vw + 1rem, 12em)",
       },
     },
   },
