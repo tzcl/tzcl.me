fadde54aecda7dab898ac829d729b7738f24507e
Toby Law <toby.law@rokt.com>
2024-05-12 11:50:42 +1000

    Update homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import remarkSidenotes from "remark-sidenotes";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,15 +12,11 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkSidenotes],
     shikiConfig: {
-      theme: "css-variables",
-    },
+      theme: "css-variables"
+    }
   },
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      // Disable injecting a basic `base.css` on every page
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind({
+    // Disable injecting a basic `base.css` on every page
+    applyBaseStyles: false
+  }), icon()]
 });
