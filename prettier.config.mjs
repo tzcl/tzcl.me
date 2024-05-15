01a070f3f968dd40bdd49add45ca2f0180f556c1
Toby Law <toby.law@rokt.com>
2024-05-15 20:15:47 +1000

    Fix up tailwind formatting
    Now classes are automatically sorted

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,3 +1,5 @@
 export default {
-  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
+  // prettier-plugin-tailwind must be last due to per-plugin workarounds
+  // https://github.com/tailwindlabs/prettier-plugin-tailwindcss?tab=readme-ov-file#compatibility-with-other-prettier-plugins
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 };
